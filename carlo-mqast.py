72be98b6050f6b1a567f25d157bf2edcc0dc2744
Leodegario U. Lorenzo II <leolorenzoii@gmail.com>
2017-02-03 15:16:36 +0800

    Monitoring Codes for Monitoring Quality Assessment for Timeliness

---
+++ carlo-mqast.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 03 15:13:59 2017
+
+@author: CRB
+"""
+
---
+++ meryll-mqast.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 03 15:12:32 2017
+
+@author: MAJV
+"""
+
