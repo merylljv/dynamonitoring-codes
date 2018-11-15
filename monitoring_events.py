d17a891dbd147714797b45382e20e2bdc029310c
merylljv <meryllviernes08@gmail.com>
2018-11-15 20:54:44 +0800

    timeliness data

--- monitoring_events.py
+++ monitoring_events.py
@@ -168,31 +168,16 @@ def get_expected_routine_release(start, end, events, releases):
     releases = releases.set_index('event_id')
     events = events.set_index('event_id')
     event_monitoring = releases.join(events)
-    event_monitoring = event_monitoring[event_monitoring.status != 'routine'][['target_release', 'site_id']]
+    event_monitoring = event_monitoring[event_monitoring.status != 'routine']
+    event_monitoring = event_monitoring[['target_release', 'site_id']]
     event_monitoring['site_id'] = event_monitoring['site_id'].apply(lambda x: int(x))
     event_tuple = list(event_monitoring.itertuples(index=False, name=None))
     routine_tuple = set(routine_tuple) - set(event_tuple)
     
-    expected_routine = pd.DataFrame(list(routine_tuple), columns=['target_release', 'site_id'])
+    expected_routine = pd.DataFrame(list(routine_tuple),
+                                    columns=['target_release', 'site_id'])
     
     return expected_routine
-
-#
-#def network_prefix(df):
-#   num = df.sim_num
-#   network_pref = num.apply(lambda x: x[-9] + x[-8])
-#   
-#   network_type = []
-#   for i in range (0, len(network_pref)):    
-#       if network_pref[i] in globe:
-#           network_type.append('globe')
-#       else: 
-#           network_type.append('smart')
-#       
-#   network_type = np.array(network_type)
-#   
-#   return network_type
-
 
 
 def get_web_releases(start, end, events):
@@ -219,14 +204,96 @@ def get_web_releases(start, end, events):
     return releases
 
 
-def get_missed_releases(releases, events):
+def get_missed_releases(releases, events, expected_routine):
+    for event_id in events[events.status == 'invalid'].event_id:
+        events.loc[events.event_id == event_id, 'validity'] = max(releases[releases.event_id == event_id].target_release)
     event_releases = releases.groupby('event_id', as_index=False)
-    missed_releases = event_releases.apply(get_missed_event_release,
-                                           events=events)
+    missed_event_releases = event_releases.apply(get_missed_event_release,
+                                                 events=events)
+    missed_event_releases = missed_event_releases[missed_event_releases['missed'].apply(lambda x: len(x)) != 0].reset_index(drop=True)
     
-    print 'missed_releases', missed_releases.reset_index()
+    monitoring_releases = releases.set_index('event_id').join(events.set_index('event_id'))
+    routine_releases = monitoring_releases[monitoring_releases.status == 'routine']
+    routine_releases = routine_releases[['target_release', 'site_id']]
+    expected_routine_tuple = list(expected_routine.itertuples(index=False, name=None))
+    released_routine_tuple = list(routine_releases.itertuples(index=False, name=None))
+    missed_routine_tuple = set(expected_routine_tuple) - set(released_routine_tuple)
+    missed_routine_releases = pd.DataFrame(list(missed_routine_tuple),
+                                           columns=['target_release', 'site_id'])
     
-    return missed_releases
+    return missed_event_releases, missed_routine_releases
+
+
+def get_expected_extended_releases(events):
+    extended_releases = events[events.status == 'finished'][['site_id', 'validity']]
+    expected_extended_release = pd.DataFrame()
+    for i in range(1,4):
+        expected_extended_release = expected_extended_release.append(pd.DataFrame({'site_id': extended_releases['site_id'], 'target_release': extended_releases['validity'].apply(lambda x: pd.datetime.combine((x + timedelta(i)).date(), time(12)))}))
+    return expected_extended_release
+
+
+def get_expected_event_releases(missed_event_releases, events, releases):
+    for i in missed_event_releases.index:
+        missed_event_releases.loc[missed_event_releases.index == i, 'extended'] = [[np.array(missed_event_releases[missed_event_releases.index == i]['missed'].values[0]) > pd.to_datetime(missed_event_releases[missed_event_releases.index == i]['event_end'].values[0])]]
+    missed_event_releases['extended'] = missed_event_releases['extended'].apply(lambda x: x[0])
+    missed_event = missed_event_releases.set_index('event_id').join(events.set_index('event_id')[['site_id']])[['site_id', 'missed', 'extended']]
+    missed_heightened_releases = pd.DataFrame()
+    for i in range(len(missed_event)):
+        partial = missed_event[i:i+1]
+        missed = np.array(partial['missed'].values[0])
+        missed = missed[~partial['extended'].values[0]]
+        if len(missed) != 0:
+            missed_heightened_releases = missed_heightened_releases.append(pd.DataFrame({'target_release': missed, 'site_id': [partial['site_id'].values[0]] * len(missed), 'event_id': [partial.index.values[0]] * len(missed)}))
+    
+    monitoring_releases = releases.set_index('event_id').join(events.set_index('event_id'))
+    
+    successful_event_releases = monitoring_releases[(monitoring_releases.status != 'routine') \
+                                                 & (monitoring_releases.validity >= monitoring_releases.target_release)][['target_release', 'site_id']]
+    
+    expected_event_releases = missed_heightened_releases.append(successful_event_releases,
+                                                                ignore_index=True)
+
+    return expected_event_releases
+
+
+def get_smsoutbox(start, end):
+    query =  "SELECT outbox_id, ts_written, ts_sent, site_code, org_name, "
+    query += "fullname, sim_num, send_status, sms_msg FROM "
+    query += "  (SELECT outbox_id, ts_written, ts_sent, sim_num, "
+    query += "  CONCAT(firstname, ' ', lastname) AS fullname, sms_msg, "
+    query += "  send_status, user_id FROM "
+    query += "    (select * FROM comms_db.smsoutbox_users "
+    query += "    WHERE sms_msg regexp 'ang alert level' "
+    query += "    ) AS outbox "
+    query += "  INNER JOIN "
+    query += "    (SELECT * FROM comms_db.smsoutbox_user_status "
+    query += "    WHERE send_status >= 5 "
+    query += "    AND ts_sent BETWEEN '%s' AND '%s' " %(start, end)
+    query += "    ) AS stat "
+    query += "  USING (outbox_id) "
+    query += "  INNER JOIN "
+    query += "    comms_db.user_mobile "
+    query += "  USING (mobile_id) "
+    query += "  INNER JOIN "
+    query += "    comms_db.users "
+    query += "  USING (user_id) "
+    query += "  ) AS msg "
+    query += "INNER JOIN "
+    query += "  (SELECT user_id, site_code, org_name FROM "
+    query += "    (SELECT * FROM comms_db.user_organization "
+    query += "    WHERE org_name in ('lewc', 'blgu', 'mlgu', 'plgu', 'pdrrmc') "
+    query += "    ) AS org "
+    query += "  INNER JOIN "
+    query += "    sites "
+    query += "  ON sites.site_id = org.fk_site_id "
+    query += "  ) AS site_org "
+    query += "USING (user_id) "
+    query += "GROUP BY site_code, org_name, sms_msg "
+    query += "ORDER BY outbox_id DESC"
+    smsoutbox = dbio.df_read(query)
+    
+    return smsoutbox
+
 
 ###############################################################################
 
@@ -235,8 +302,13 @@ if __name__ == '__main__':
     start = '2018-01-01'
     end = '2018-10-31 23:59:59'
     
+    # web releases
     events = get_event_profile(start, end)
     releases = get_web_releases(start, end, events)
+    expected_routine_releases = get_expected_routine_release(start, end, events, releases)
+    missed_event_releases, missed_routine_releases = get_missed_releases(releases, events, expected_routine_releases)
+    expected_event_releases = get_expected_event_releases(missed_event_releases, events, releases)
+    expected_extended_releases = get_expected_extended_releases(events)
     
-    expected_routine = get_expected_routine_release(start, end, events, releases)
-    missed_releases = get_missed_releases(releases, events)
+    # EWI
+    smsoutbox = get_smsoutbox(start, end)
